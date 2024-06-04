26f36003dca241d0fe88a00bcdb6c0b2b5d8d303
cstmrtn <153620020+cstmrtn@users.noreply.github.com>
2024-06-04 19:42:29 +0200

    Update SajatProjekt.py

--- SajatProjekt.py
+++ SajatProjekt.py
@@ -14,15 +14,31 @@ def main(page: ft.Page):
     page.title = 'Bejelentkezés'
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.theme_mode = ft.ThemeMode.DARK
     
 
-    t1 = ft.Text(color="red")
     tb1: TextField = TextField(label="Username", width=350, border_color="red", cursor_color="red",)
     tb2: TextField = TextField(label="Password", width=350, password=True,
     border_color="red", can_reveal_password=True, cursor_color="red")
     button: ElevatedButton = ElevatedButton(text="OK", color="red", disabled=True)
-    cont1 = ft.Container(t1, height=150, width=300,padding=20, bgcolor=ft.colors.BLACK)
+    button2: ElevatedButton = ElevatedButton(text="TOVÁBB", color="red")
+
+    img = ft.Image(
+        src=f"/Users/csataimartin/Documents/mobile_application/projektek/icons/emmanuel-jdz.gif",
+        width=500,
+        height=500,
+        fit=ft.ImageFit.CONTAIN,
+        )
+    images = ft.Row(expand=1, wrap=False, scroll="always")
+    
+    def submit2(e:ControlEvent) ->None:
+        page.clean()
+
+        page.add(
+            img,images
+        )
+        
+
+
 
     def validate(e:ControlEvent) ->None:
         if all ([tb1.value, tb2.value]):
@@ -33,21 +49,20 @@ def main(page: ft.Page):
         page.update()
 
     def submit(e: ControlEvent) ->None:
-        print('Username:', tb1.value)
-        print('Password:', tb2.value)
-
+    
         page.clean()
 
         page.add(
             Row(
                 controls = [Text(value=f'Üdvozollek: {tb1.value} \n A jelszavad: {tb2.value}', size=20)],
                 alignment= ft.MainAxisAlignment.CENTER 
-            )
+            ), button2
         )
     
     tb1.on_change = validate
     tb2.on_change = validate
     button.on_click = submit
+    button2.on_click = submit2
 
     page.add(
         Row(
